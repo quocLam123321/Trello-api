0e18d5b0213b86ed8b68df9e945687649ed406da
Quốc Lâm <lamquoc07122004@gmail.com>
2025-11-25 16:12:15 +0700

    config route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { Router } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const router = Router()
+
+router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'Hello from API v1' })
+  })
+
+// board routes
+router.use('/boards', boardRoutes)
+
+export const API_V1 = router
